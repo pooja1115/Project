ef71e25381b7b6528d32cbd5beba7a1b6d524799
pooja jaras <86953923+pooja1115@users.noreply.github.com>
2021-10-05 12:11:07 +0530

    Add files via upload

---
+++ Core java project/game.java
@@ -0,0 +1,227 @@
+
+import java.util.Scanner;   
+import java.util.*;
+public class game
+{   
+public static void main(String[] args)   
+{   
+	char ch2;
+Scanner sc = new Scanner(System.in);   
+
+System.out.print("Enter alphabate: "); 
+char ch =sc.next().charAt(0);
+
+
+
+switch(ch)
+{
+case 'a': 
+	System.out.println("|-------***------|");
+	System.out.println("|  A for Apple.  |"); 
+	System.out.println("|-------***------|");
+	System.out.println("|  A for Ant.    |");
+	System.out.println("|-------***------|");
+	break;
+	
+case 'b':
+	System.out.println("|-------***------|");
+	System.out.println("|  B for Ball.   |");
+	System.out.println("|-------***------|");
+	System.out.println("|  B for Bat.    |"); 
+	System.out.println("|-------***------|");
+	break;
+	
+case 'c':
+	System.out.println("|-------***------|");
+	System.out.println("|  C for cat.    |"); 
+	System.out.println("|-------***------|");
+	System.out.println("|  C for camel.  |"); 
+	System.out.println("|-------***------|");
+	break;
+	
+case 'd':
+	System.out.println("|-------***------|");
+	System.out.println("|  D for dog.    |"); 
+	System.out.println("|-------***------|");
+	System.out.println("|  D for duck.   |"); 
+	System.out.println("|-------***------|");
+	break;
+	
+case 'e':
+	System.out.println("|-------***------|");
+	System.out.println("|  E for Egg.    |"); 
+	System.out.println("|-------***------|");
+	System.out.println("|  E for Egle.   |"); 
+	System.out.println("|-------***------|");
+	break;
+	
+case 'f' :
+	System.out.println("|-------***------|");
+	System.out.println("|  F for Fan.    |"); 
+	System.out.println("|-------***------|");
+	System.out.println("|  F for  Fish.  |");
+	System.out.println("|-------***------|");
+	break;
+	
+case 'g':
+	System.out.println("|-------***------|");
+	System.out.println("|  G for Girl.   |"); 
+	System.out.println("|-------***------|");
+	System.out.println("|  G for  Got.   |"); 
+	System.out.println("|-------***------|");
+	break;
+	
+case 'h':
+	System.out.println("|-------***------|");
+	System.out.println("|  H for Hen.    |"); 
+	System.out.println("|-------***------|");
+	System.out.println("|  H for Hut.    |"); 
+	System.out.println("|-------***------|");
+	break;
+	
+case 'i':
+	System.out.println("|-------***------|");
+	System.out.println("|  I for Ice.    |"); 
+	System.out.println("|-------***------|");
+	System.out.println("|  I for Iron.   |"); 
+	System.out.println("|-------***------|");
+	break;
+	
+case'j':
+	System.out.println("|-------***------|");
+	System.out.println("|  J for Joker.  |"); 
+	System.out.println("|-------***------|");
+	System.out.println("|  J for Jug.    |"); 
+	System.out.println("|-------***------|");
+	break;
+	
+case 'k':
+	System.out.println("|-------***------|");
+	System.out.println("|  K for King.   |"); 
+	System.out.println("|-------***------|");
+	System.out.println("|  K for Kite.   |"); 
+	System.out.println("|-------***------|");
+	break;
+	
+case 'l':
+	System.out.println("|-------***------|");
+	System.out.println("|  L for Leg.    |"); 
+	System.out.println("|-------***------|");
+	System.out.println("|  L for Lion.   |"); 
+	System.out.println("|-------***------|");
+	break;
+	
+case'm':
+	System.out.println("|-------***------|");
+	System.out.println("|  M for Man.    |"); 
+	System.out.println("|-------***------|");
+	System.out.println("|  M for Monkey. |"); 
+	System.out.println("|-------***------|");
+	break;
+	
+case'n':
+	System.out.println("|-------***------|");
+	System.out.println("|  N for Nest.   |"); 
+	System.out.println("|-------***------|");
+	System.out.println("|  N for Nose.   |");
+	System.out.println("|-------***------|");
+	break;
+	
+case 'o':
+	System.out.println("|-------***------|");
+	System.out.println("|  O for Orange. |"); 
+	System.out.println("|-------***------|");
+	System.out.println("|  O for Old.    |"); 
+	System.out.println("|-------***------|");
+	break;
+	
+case 'p':
+	System.out.println("|-------***------|");
+	System.out.println("|  P for Peacock.|"); 
+	System.out.println("|-------***------|");
+	System.out.println("|  P for  Pen    |");
+	System.out.println("|-------***------|");
+	break;
+	
+case 'q':
+	System.out.println("|-------***------|");
+	System.out.println("|  Q for Queen   |"); 
+	System.out.println("|-------***------|");
+	System.out.println("|  Q for Quick.  |"); 
+	System.out.println("|-------***------|");
+	break;
+	
+case 'r':
+	System.out.println("|-------***------|");
+	System.out.println("|  R for RAt.    |"); 
+	System.out.println("|-------***------|");
+	System.out.println("|  R for Rabbit. |"); 
+	System.out.println("|-------***------|");
+	break;
+	
+case's':
+	System.out.println("|-------***------|");
+	System.out.println("|  S for Sun     |"); 
+	System.out.println("|-------***------|");
+	System.out.println("|  S for  Soup   |"); 
+	System.out.println("|-------***------|");
+	break;
+	
+case 't':
+	System.out.println("|-------***------|");
+	System.out.println("| T for Ten.     |"); 
+	System.out.println("|-------***------|");
+	System.out.println("| T for Tiger.   |");
+	System.out.println("|-------***------|");
+	break;
+	
+case 'u':
+	System.out.println("|-------***------|");
+	System.out.println("| U for Umbrella.|"); 
+	System.out.println("|-------***------|");
+	System.out.println("| U for Uncle.   |"); 
+	System.out.println("|-------***------|");
+	break;
+	
+case 'v':
+	System.out.println("|-------***------|");
+	System.out.println("| V for van.     |"); 
+	System.out.println("|-------***------|");
+	System.out.println("| V for voice.   |"); 
+	System.out.println("|-------***------|");
+	break;
+	
+case 'w':
+	System.out.println("|-------***------|");
+	System.out.println("| W for Wax.     |"); 
+	System.out.println("|-------***------|");
+	System.out.println("| W for Wet.     |"); 
+	System.out.println("|-------***------|");
+	break;
+case 'x':
+	System.out.println("|-------***------|");
+	System.out.println("| X for x-mas tree.|");
+	System.out.println("|-------***------|");
+	System.out.println("| X for x-ray.   |"); 
+	System.out.println("|-------***------|");
+	break;
+case 'y':
+	System.out.println("|-------***------|");
+	System.out.println("|   Y for Yak.   |"); 
+	System.out.println("|-------***------|");
+	System.out.println("|  Y for Yawn.   |");
+	System.out.println("|-------***------|");
+	break;
+	
+	case 'z':
+		System.out.println("|-------***------|");
+		System.out.println("| Z for Zebra.   |"); 
+		System.out.println("|-------***------|");
+		System.out.println("| Z for Zoo.     |"); 
+		System.out.println("|-------***------|");
+		break;
+	
+}
+
+}
+}
